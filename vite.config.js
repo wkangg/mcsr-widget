1775b7bbba6350735f53600c9cc2c094a92dc59f
William Kang <william@wkang.ca>
2025-10-05 23:17:12 -0400

    test

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // publicPath: '/mcsr-widget/',
   base: '/mcsr-widget/',
   transpileDependencies: true,
   plugins: [vue(), vueDevTools()],
