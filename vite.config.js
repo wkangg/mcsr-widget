fd0294ca11861d50af514d125f8e12b46fe9193f
osh <oshfour@gmail.com>
2025-10-05 22:24:11 -0400

    asdasdasds

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,13 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import svgLoader from 'vite-svg-loader'
 
-const newLocal = '/'
 // https://vite.dev/config/
 export default defineConfig({
-  // publicPath: newLocal,
-  base: process.env.NODE_ENV === 'production' ? '/mcsr-widget/' : '/',
-  transpileDependencies: true,
-  plugins: [vue(), vueDevTools()],
+  plugins: [vue(), vueDevTools(), svgLoader()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
