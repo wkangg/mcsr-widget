d21dbdb50991084577e2556155c801c3f3b8648d
William Kang <william@wkang.ca>
2025-10-05 23:35:34 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,12 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import svgLoader from 'vite-svg-loader'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/mcsr-widget/',
-  plugins: [vue(), vueDevTools()],
+  plugins: [vue(), vueDevTools(), svgLoader()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
